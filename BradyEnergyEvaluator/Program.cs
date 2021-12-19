cb8fcd7251d3e774dc6967a447ff03d3db5f4c72
Ed OB <ed@ed.com>
2021-12-19 13:04:15 +0000

    fixed looping issue

--- BradyEnergyEvaluator/Program.cs
+++ BradyEnergyEvaluator/Program.cs
@@ -14,12 +14,12 @@ namespace BradyEnergyEvaluator
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             // get folder locations
             string inputFolder = ConfigurationManager.AppSettings["inputFolder"];
             string outputFolder = ConfigurationManager.AppSettings["outputFolder"];
+            List<string> processedReports = new List<string>();
 
             Console.WriteLine("Awaiting File Drop");
 
@@ -32,44 +32,64 @@ namespace BradyEnergyEvaluator
                 NotifyFilter = NotifyFilters.LastWrite
             };
 
-            fileWatcher.Changed += (sender, eventArgs) => AnalyseReports(inputFolder, outputFolder, referenceData);
+            // File watcher is flawed and throws multiple events on file changes, a better tool decision would have been better
+            fileWatcher.Changed += (sender, eventArgs) => AnalyseReports(inputFolder, outputFolder, referenceData, processedReports);
 
-            //get all files in a folder
-            //cAnalyseReports(inputFolder, outputFolder, referenceData);
             Console.ReadLine();
         }
 
-        private static void AnalyseReports(string inputFolder, string outputFolder, ReferenceData referenceData)
+
+
+        private static void AnalyseReports(string inputFolder, string outputFolder, ReferenceData referenceData, List<string> processedReports)
         {
-            try {
-                IEnumerable<string> reportsToBeProcessed = Directory.GetFiles(inputFolder, "*.xml");
+            IEnumerable<string> reportsToBeProcessed = Directory.GetFiles(inputFolder, "*.xml");
 
-                Console.WriteLine("Files Found");
-
-                foreach (string fileName in reportsToBeProcessed)
-                {
-                    GenerationReports report = getReport(fileName);
-
-                    var output = ReportAnalyser.AnalyseReport(referenceData, report);
-
-                    var writer = new XmlSerializer(typeof(GenerationOutput));
-                    var inputFileName = Path.GetFileNameWithoutExtension(fileName);
-
-                    var file = File.Create($"{outputFolder}\\{inputFileName}-result.xml");
-                    writer.Serialize(file, output);
-                    file.Close();
-
-                    Console.WriteLine("Report Created");
-                }
-            } catch (IOException e)
+            foreach  (string fileName in reportsToBeProcessed)
             {
-                // TO DO find a more graceful way to wait for file to finish copying
-                Console.WriteLine("Awaiting File to become unlocked");
-                // TO DO Readline stops the recursive loop of constant accessing and failure
-               // Console.ReadLine();
-                AnalyseReports(inputFolder, outputFolder, referenceData);
-
+                // Due to file watcher throwing multiple events for a transaction, need to check the file has not already been changed in session
+                if (!processedReports.Contains(fileName))
+                {
+                    while (true)
+                    {
+                        // To avoid locking of file, need to check that file is free
+                        if (!IsFileLocked(fileName))
+                        {
+                            AnalyseReport(outputFolder, referenceData, fileName);
+                            processedReports.Add(fileName);
+                            break;
+                        };
+                    }
+                }
             }
+        }
+
+        private static bool IsFileLocked(string filePath)
+        {
+            var ret = false;
+            try
+            {
+                using (File.Open(filePath, FileMode.Open)) { }
+            }
+            catch (IOException e)
+            {
+            }
+            return ret;
+        }
+
+        private static void AnalyseReport(string outputFolder, ReferenceData referenceData, string fileName)
+        {
+            GenerationReports report = getReport(fileName);
+
+            var output = ReportAnalyser.AnalyseReport(referenceData, report);
+
+            var writer = new XmlSerializer(typeof(GenerationOutput));
+            var inputFileName = Path.GetFileNameWithoutExtension(fileName);
+
+            var file = File.Create($"{outputFolder}\\{inputFileName}-result.xml");
+            writer.Serialize(file, output);
+            file.Close();
+
+            Console.WriteLine($"Report Created - {inputFileName}-result.xml");
         }
 
         private static GenerationReports getReport(string fileName)
